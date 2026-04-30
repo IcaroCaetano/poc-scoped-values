b2e6d94cec847bd456b1566688fc471afbf9f3e9
ICAROCAETANO <icaro.caetano@gmail.com>
2026-04-30 18:42:19 -0300

    first commit

---
+++ src/main/java/com/myprojecticaro/poc_scoped_values/PocScopedValuesApplication.java
@@ -0,0 +1,13 @@
+package com.myprojecticaro.poc_scoped_values;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocScopedValuesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocScopedValuesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/myprojecticaro/poc_scoped_values/PocScopedValuesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myprojecticaro.poc_scoped_values;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocScopedValuesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
