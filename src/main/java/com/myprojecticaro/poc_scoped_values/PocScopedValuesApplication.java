765f73ef6000f6e7117b98e2a09110bee535dbfd
ICAROCAETANO <icaro.caetano@gmail.com>
2026-04-30 18:48:47 -0300

    feat: remove sping from poc

--- src/main/java/com/myprojecticaro/poc_scoped_values/PocScopedValuesApplication.java
+++ src/main/java/com/myprojecticaro/poc_scoped_values/PocScopedValuesApplication.java
@@ -1,13 +1,11 @@
 package com.myprojecticaro.poc_scoped_values;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
 public class PocScopedValuesApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PocScopedValuesApplication.class, args);
+
+		System.out.println("poc-scoped-values started");
 	}
 
 }
