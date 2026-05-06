2a91c2e0b333304f1701278075322d662f7269d1
ICAROCAETANO <icaro.caetano@gmail.com>
2026-05-05 22:37:32 -0300

    feat: add context

---
+++ src/main/java/com/myprojecticaro/poc_scoped_values/api/context/ScopedContext.java
@@ -0,0 +1,14 @@
+package com.myprojecticaro.poc_scoped_values.api.context;
+
+public final class ScopedContext {
+
+    private ScopedContext() {}
+
+    public static final ScopedValue<RequestContext> CONTEXT =
+            ScopedValue.newInstance();
+
+    public static RequestContext get() {
+
+        return CONTEXT.get();
+    }
+}
