0b3aa1edc874a979219422befcf6cee8b0d3882d
ICAROCAETANO <icaro.caetano@gmail.com>
2026-05-12 22:21:39 -0300

    feat:add  propagation experiment main

---
+++ src/main/java/com/myprojecticaro/poc_scoped_values/propagation/PropagationMain.java
@@ -0,0 +1,43 @@
+package com.myprojecticaro.poc_scoped_values.propagation;
+
+import com.myprojecticaro.poc_scoped_values.virtualthreads.context.RequestContext;
+import com.myprojecticaro.poc_scoped_values.virtualthreads.context.ScopedRequestContext;
+
+import java.lang.ScopedValue;
+import java.util.UUID;
+
+public class PropagationMain {
+
+    public static void main(String[] args) throws Exception {
+
+        var context = new RequestContext(
+                "icaro",
+                UUID.randomUUID().toString()
+        );
+
+        ScopedValue.where(
+                ScopedRequestContext.CONTEXT,
+                context
+        ).run(() -> {
+
+            try {
+
+                new StructuredPropagationExample()
+                        .execute();
+
+                new ManualPropagationExample()
+                        .execute();
+
+                new ThreadSwitchingExperiment()
+                        .execute();
+
+                new BrokenPropagationExample()
+                        .execute();
+
+            } catch (Exception e) {
+
+                e.printStackTrace();
+            }
+        });
+    }
+}
